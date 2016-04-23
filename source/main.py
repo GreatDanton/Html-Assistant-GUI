3f425c4dce80ae20831d55578de48071db72a44b
GreatDanton <pribosek.jan@gmail.com>
2016-04-23 12:53:28 +0200

    added toolbar version 0.1.0

--- source/main.py
+++ source/main.py
@@ -1,9 +1,48 @@
+#
+#  ======= HTML ASSISTANT ========
+#
+# Author: Jan Pribosek
+# Version: 0.1.0
+#
+# ======= LICENSE =======
+#
+"""
+MIT License
+
+Copyright (c) [2016] [Jan Pribosek]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
 import gi
 import os
+import webbrowser
+import json
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 filesPath = [] 
+documentation_url = 'http://greatdanton.github.io/Html-Assistant-GUI/documentation.html'
+DATA = ""
+TEMPLATES = []
+PROGRAM_DIRECTORY = os.getcwd()
+
 
 def replace(folder, search_for):
     # search in directory
@@ -18,7 +57,6 @@ def replace(folder, search_for):
         if file.endswith(".html"):
             html_files.append(file)
 
-    print(html_files)
 
 # get text from navigation_editor
     editor = builder.get_object("navigation_editor")
@@ -99,17 +137,224 @@ def replace(folder, search_for):
 
 class Handler:
 
+# main menu button click handlers
 
-    def selectFolder(self, button):
+##### NEW FILE MENU & MODAL HANDLERS #####
+
+# opens newFile modal when newFile icon is clicked
+    def menu_click_newFile(self, button):
+        newFile = builder.get_object("newFileModal")
+
+# if path exist change directory to last opened project
+        if ('/' in DATA["path"]):
+            os.chdir(DATA["path"])
+
+        newFile.set_current_name("Untitled.html")
+        response = newFile.run()
+        if response == -4:
+            newFile.hide()
+
+
+    def newFileModal_save_click(self, button):
+        newFileModal = builder.get_object("newFileModal")
+        fileName = newFileModal.get_current_name()
+
+        if os.path.isfile(fileName):
+            msgModal = builder.get_object("msg_fileAlreadyExist")
+            modal_response = msgModal.run()
+            if modal_response == -4:
+                msgModal.hide()
+        else:
+            output_file = open(fileName, 'x')
+# write to output file => add from template
+            output_file.close()
+
+
+##### EDIT TEMPLATE MENU & MODAL  HANDLERS ######
+
+# opens editTemplate modal when editTemplate icon is clicked
+    def menu_click_editTemplate(self, button):
+        editTemplate = builder.get_object("editTemplateModal")
+        templateTreeView = builder.get_object("templateTreeView")
+        templateList = builder.get_object("templateStorage")
+
+# load json file (to read templates)
+        with open(PROGRAM_DIRECTORY + '/config.json') as data_file:
+            DATA = json.load(data_file)
+
+        templateList.clear()
+        
+        for template in DATA["templates"]:
+            templateList.append([template])
+            TEMPLATES.append(template)
+
+        if len(TEMPLATES) >= 1:
+# select first row
+            templateTreeView.set_cursor(0)
+
+
+        response = editTemplate.run()
+        if response == -4:
+            editTemplate.hide()
+    
+# add template
+    def editTemplate_addTemplate_click(self, button):
+        templateTreeView = builder.get_object("templateTreeView")
+        templateList = builder.get_object("templateStorage")
+
+# add to template list
+        all_files = [];
+        for row in templateList:
+            all_files.append(row[:])
+    
+# flatten all_files array 
+        all_files = [item for sublist in all_files for item in sublist]
+
+# if untitled name exist in the all_files array add - number
+        name = 'Untitled'
+        i = 1
+
+        while (name in all_files):
+            name = 'Untitled' + '-' + str(i)
+            i += 1
+
+# add new name to the View & update text box
+        templateList.append([name])
+        tb_templateName = builder.get_object("editTemplate_templateName")
+        tb_templateName.set_text(name)
+       
+# select/focus last created element
+        path = Gtk.TreePath(len(all_files))
+        templateTreeView.set_cursor(path, None)
+
+# when template is added, empty editor window
+        template_editor = builder.get_object("editTemplate_editor")
+        template_editor.get_buffer().set_text("")        
+
+
+# delete template
+    def editTemplate_deleteTemplate_click(self, button):
+        templateTreeView = builder.get_object("templateTreeView")
+        selected = templateTreeView.get_selection()
+        selected = selected.get_selected()
+
+# remove selected row
+        templateList = builder.get_object("templateStorage")
+
+        print(selected[1])
+        model, treeiter = selected
+        if treeiter != None:
+            delete_template_name = model[treeiter][0]
+
+# update TEMPLATES and write DATA to config.json
+        if delete_template_name in TEMPLATES:
+            index = TEMPLATES.index(delete_template_name)
+            del TEMPLATES[index]
+            del DATA["templates"][delete_template_name]
+           
+            with open(PROGRAM_DIRECTORY + "/config.json", "w") as outfile:
+                json.dump(DATA, outfile, indent=4)
+
+# remove selected deleted item from templateList
+        if selected[1] != None:
+            templateList.remove(selected[1])
+        else:
+            tb_templateName = builder.get_object("editTemplate_templateName")
+            tb_templateName.set_text("")
+
+# save template
+    def editTemplate_saveTemplate_click(self, button):
+
+        templateTreeView = builder.get_object("templateTreeView")
+        templateList = builder.get_object("templateStorage")
+        editor = builder.get_object("editTemplate_editor")
+        tb_templateName = builder.get_object("editTemplate_templateName")
+
+        selected = templateTreeView.get_selection()
+        selected = selected.get_selected()
+
+        model, treeiter = selected
+        if treeiter !=None:
+            name = model[treeiter][0]
+        
+        selected = name
+
+# get text from templateName text box
+        new_name = tb_templateName.get_text()
+
+# get text from editor window
+        start_iter = editor.get_buffer().get_start_iter()
+        end_iter = editor.get_buffer().get_end_iter()
+        editor_text = editor.get_buffer().get_text(start_iter, end_iter, True)
+     
+        DATA["templates"][new_name] = editor_text
+
+        if selected in TEMPLATES:
+            del DATA["templates"][selected]
+            DATA["templates"][new_name] = editor_text
+
+            with open(PROGRAM_DIRECTORY + "/config.json", "w") as outfile:
+                json.dump(DATA, outfile, indent=4)
+
+# updating TEMPLATES (replace old name with new one)
+            templates_index = TEMPLATES.index(selected)
+            TEMPLATES[templates_index] = new_name
+            
+            templateList[treeiter][0] = new_name
+# UPDATE VIEW
+        else:
+            DATA["templates"][new_name] = editor_text
+            with open(PROGRAM_DIRECTORY + "/config.json", "w") as outfile:
+                json.dump(DATA, outfile, indent=4)
+# updating TEMPLATES and treeView
+            TEMPLATES.append(new_name)
+            templateList[treeiter][0] = new_name
+        
+
+    def templateTreeView_row_click(self, selection):
+        templateTreeView = builder.get_object("templateTreeView")
+        templateEditor = builder.get_object("editTemplate_editor")
+
+        selected = selection.get_selection()
+        selected = selected.get_selected()
+        
+        tb_templateName = builder.get_object("editTemplate_templateName")
+
+        model, treeiter = selected
+        if treeiter != None:
+            fileName = model[treeiter][0]
+            tb_templateName.set_text(fileName)
+
+        if fileName in TEMPLATES:
+            templateEditor.get_buffer().set_text(DATA["templates"][fileName])    
+        else:
+            templateEditor.get_buffer().set_text("")
+
+# opens web browser when help icon is clicked
+    def menu_click_help(self, button):
+        webbrowser.open(documentation_url)
+
+
+# open about modal when about icon is clicked
+    def menu_click_about(self, button):
+        about_modal = builder.get_object("about_modal")
+        response = about_modal.run()
+        if response:
+            about_modal.hide()
+
+
+    def menu_click_openProject(self, button):
         folderChooserModal = builder.get_object("folderChooserModal")
         response = folderChooserModal.run()
         openBtn = builder.get_object("openFolderBtn")
         if response == 1:
 
 # get files path and append it to filesPath array
-            filesPath1 = folderChooserModal.get_uri()
-            filesPath1 = filesPath1.replace("file://", "")
+            filesPath1 = folderChooserModal.get_filename()
             filesPath.append(filesPath1)
+
+            tb_projectPath = builder.get_object("tb_projectPath")
+            tb_projectPath.set_text(filesPath1)
 
 # append .html files into files tree view
             filesTreeView =  builder.get_object("filesTreeView")
@@ -128,6 +373,11 @@ class Handler:
             for file in html_files:
                 htmlFilesList.append([file])
 
+# store project path into config.json
+            DATA["path"] = filesPath[-1]
+            with open("config.json", "w") as outfile:
+                json.dump(DATA, outfile, indent=4)
+            
 # hides openFolderDialog
             folderChooserModal.hide()
         else:
@@ -135,16 +385,18 @@ class Handler:
 
 
     def updateNavigation(self, button):
-        print(filesPath)
         replaceNav = builder.get_object("replaceNavBox")
         search_for = replaceNav.get_buffer().get_text()
-
-        replace(filesPath[-1], search_for)
+        
+        projectPath = builder.get_object("tb_projectPath")
+        path = projectPath.get_text()
+        replace(path, search_for)
 
 
 # when x is clicked, program exit
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
+
 
 
 # load css styling
@@ -167,9 +419,26 @@ builder.add_from_file("assistant.glade")
 builder.connect_signals(Handler())
 
 #showing window
-window = builder.get_object("window1")
+window = builder.get_object("mainWindow")
 window.set_title("Html Assistant")
 window.show_all()
+
+
+
+# load last project path from config.json, add path to
+# project path textbox and add files to TreeView
+with open('config.json') as data_file:
+    DATA = json.load(data_file)
+
+tb_projectPath = builder.get_object("tb_projectPath")
+tb_projectPath.set_text(DATA["path"])
+
+html_files = os.listdir(DATA["path"])
+htmlFilesList = builder.get_object("htmlFilesList")
+for file in html_files:
+    htmlFilesList.append([file])
+
+
 
 # show Files column in Tree View
 for i, col_title in enumerate(["Files:"]):
@@ -177,5 +446,14 @@ for i, col_title in enumerate(["Files:"]):
     filesTreeView = builder.get_object("filesTreeView")
     column = Gtk.TreeViewColumn(col_title, renderer, text=i)
     filesTreeView.append_column(column)
+
+
+# add text templates in templates tree view
+for i, col_title in enumerate(["Templates:"]):
+    renderer = Gtk.CellRendererText()
+    templateTreeView = builder.get_object("templateTreeView")
+    column = Gtk.TreeViewColumn(col_title, renderer, text=i)
+    templateTreeView.append_column(column)
+
 
 Gtk.main()
