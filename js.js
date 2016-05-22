dfa1c34830347cddbe34e4540eddc4a0a0a3d939
GreatDanton <pribosek.jan@gmail.com>
2016-05-22 22:08:22 +0200

    added new navigation bar

--- js.js
+++ js.js
@@ -11,4 +11,30 @@ $(document).ready(function() {
       scrollTop: $('#' + id).offset().top - 40}, 'medium');
     });
 
+  $('.icon-menu').click(function() {
+    if ($('.mobile-modal').hasClass('mobile-modal-visible')) {
+      $('.mobile-modal').removeClass('mobile-modal-visible');
+    } else {
+      $('.mobile-modal').addClass('mobile-modal-visible');
+    }
+  });
+
+  var nav_top;
+
+  if ($('.mobile').is(':hidden')) {
+    nav_top = $('.navigation').offset().top;
+  } else {
+    nav_top = $('.mobile').offset().top;
+  }
+
+	$(document).on("scroll", function() {
+    // magic number, navigation 160px from top
+		if ($('.navigation').offset().top > nav_top) {
+      // console.log($('.navigation').offset().top);
+      $('.navigation').addClass('nav-box-shadow');
+		} else {
+			$('.navigation').removeClass('nav-box-shadow');
+		}
+	});
+
 });
